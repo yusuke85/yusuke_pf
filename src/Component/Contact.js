43f093e43b4b291471079a6fd4e381359dd32bdc
Yusuke <yusuke850208@gmail.com>
2022-02-20 11:06:54 +0100

    comentando message variabre

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -38,7 +38,7 @@ const Contact = () => {
       })
   }
 
-  const message = watch('message') || "";
+  // const message = watch('message') || "";
   // const messageCharsLeft = 1500 - message.length;
 
   return (
